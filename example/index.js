56614a1ba9f57942a4f7f4f1645422c637af3242
日天 <gaoryrt@users.noreply.github.com>
2018-06-14 11:46:03 +0800

    🎨 flip animation success

--- example/index.js
+++ example/index.js
@@ -1,17 +1,36 @@
-import Flip from '../flip'
+import './main.css'
 
-const $ = n => document.querySelector(n)
+const duration = 4000
+const dest = 32
 
-const el = new Flip({
-  node: $('.flip'),
-  from: 9527,
-  to: 42,
-  duration: 2000,
-  delay: 2000
+
+const elem = document.querySelector('.ctnr')
+
+
+// const ease = pos => {
+//   if ((pos/=0.5) < 1) return 0.5*Math.pow(pos,3);
+//   return 0.5 * (Math.pow((pos-2),3) + 2);
+// }
+
+const easeFn = pos => (pos /= .5) < 1
+  ? .5 * Math.pow(pos, 3)
+  : .5 * (Math.pow((pos - 2), 3) + 2)
+const draw = percent => {
+  const translateY = - easeFn(percent) * 50 * dest % 500
+  elem.style.transform = `translateY(${translateY}px)`
+}
+const done = () => {
+  elem.style.transform = `translateY(${50 * dest % 500}px)`
+}
+
+
+
+
+const start = performance.now()
+requestAnimationFrame(function tick(now) {
+  let elapsed = now - start
+  draw(elapsed / duration)
+  if (elapsed < duration) requestAnimationFrame(tick)
+  else done()
 })
 
-$('#shuffle').addEventListener('click', () => {
-  const num = (Math.random() * 10000).toFixed(0)
-  $('.num').innerText = num
-  el.flipTo(num)
-})
--- flip.js
+++ flip.js
@@ -1,81 +1,87 @@
-'use strict';
+// const r = require('html-text-generator')
+const g = require('gelerator')
+const { css } = require('emotion')
 
-var _templateObject = _taggedTemplateLiteralLoose(['\n  display: flex;\n'], ['\n  display: flex;\n']),
-    _templateObject2 = _taggedTemplateLiteralLoose(['\n  background-image: url(', ');\n  width: .77em;\n  height: 1em;\n  background-size: 1em 10em;\n  background-repeat: repeat;\n  top: 0;\n  left: 0;\n'], ['\n  background-image: url(', ');\n  width: .77em;\n  height: 1em;\n  background-size: 1em 10em;\n  background-repeat: repeat;\n  top: 0;\n  left: 0;\n']),
-    _templateObject3 = _taggedTemplateLiteralLoose(['\n      will-change: auto;\n      transition: background-position ', 'ms;\n    '], ['\n      will-change: auto;\n      transition: background-position ', 'ms;\n    ']);
+// const nodeClass = css`
+//   display: flex;
+// `
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+// let willChange
+// const digiClass = css`
+//   background-image: url(${require('./zero2nine.png')});
+//   width: .77em;
+//   height: 1em;
+//   background-size: 1em 10em;
+//   background-repeat: repeat;
+//   top: 0;
+//   left: 0;
+// `
 
-function _taggedTemplateLiteralLoose(strings, raw) { strings.raw = raw; return strings; }
+// const maxLenNum = (aNum, bNum) => (aNum > bNum ? aNum : bNum).toString().length
 
-var r = require('html-text-generator');
 
-var _require = require('emotion'),
-    css = _require.css;
+// const num2PadNumArr = (num, len) => {
+//   const padLeftStr = (rawStr, lenNum) => (rawStr.length < lenNum ?
+//     padLeftStr('0' + rawStr, lenNum) :
+//     rawStr)
+//   const str2NumArr = rawStr => rawStr.split('').map(Number)
+//   return str2NumArr(padLeftStr(num.toString(), len))
+// }
 
-var nodeClass = css(_templateObject);
-
-var willChange = void 0;
-var digiClass = css(_templateObject2, require('./zero2nine.png'));
-
-var maxLenNum = function maxLenNum(aNum, bNum) {
-  return (aNum > bNum ? aNum : bNum).toString().length;
-};
-
-var num2PadNumArr = function num2PadNumArr(num, len) {
-  var padLeftStr = function padLeftStr(rawStr, lenNum) {
-    return rawStr.length < lenNum ? padLeftStr('0' + rawStr, lenNum) : rawStr;
-  };
-  var str2NumArr = function str2NumArr(rawStr) {
-    return rawStr.split('').map(Number);
-  };
-  return str2NumArr(padLeftStr(num.toString(), len));
-};
-
-module.exports = function () {
-  function flip(_ref) {
-    var _this = this;
-
-    var node = _ref.node,
-        from = _ref.from,
-        to = _ref.to,
-        _ref$duration = _ref.duration,
-        duration = _ref$duration === undefined ? 1000 : _ref$duration,
-        _ref$delay = _ref.delay,
-        delay = _ref$delay === undefined ? 4 : _ref$delay;
-
-    _classCallCheck(this, flip);
-
-    willChange = css(_templateObject3, duration);
-    node.classList.add(nodeClass);
-    this.maxLen = maxLenNum(from, to || 0);
-    this.node = node;
-    this.displaing = num2PadNumArr(from, this.maxLen);
-    this.from = from;
-    this.initDigis();
-    if (to) setTimeout(function () {
-      _this.flipTo(to);
-    }, delay);
+module.exports = class flip {
+  constructor({ node, from, to, duration = 1000, delay = 4 }) {
+    willChange = css`
+      will-change: auto;
+    `
+    node.classList.add(willChange)
+    this.maxLen = maxLenNum(from, to || 0)
+    this.node = node
+    this.displaying = num2PadNumArr(from, this.maxLen)
+    this.from = from
+    this.initDigis()
+    if (to) setTimeout(() => {
+      this.flipTo(to)
+    }, delay)
   }
 
-  flip.prototype.initDigis = function initDigis() {
-    for (var i = 0; i < this.maxLen; i += 1) {
-      var iNode = r(digiClass)();
-      var offset = Math.floor(this.from / Math.pow(10, this.maxLen - 1 - i));
-      iNode.style.backgroundPosition = 'center -' + offset % 100 + 'em';
-      iNode.classList.add(willChange);
-      this['digi' + i] = iNode;
-      this.node.appendChild(iNode);
-    }
-  };
+  initDigis() {}
 
-  flip.prototype.flipTo = function flipTo(num) {
-    var numArr = num2PadNumArr(num, this.maxLen);
-    for (var i = 0; i < this.maxLen; i += 1) {
-      var offset = Number(numArr.slice(0, i + 1).join(''));
-      this['digi' + i].style.backgroundPosition = 'center -' + offset % 100 + 'em';
-    }
-  };
+  flipTo() {}
+}
 
-  return flip;
-}();
+// module.exports = class flip {
+//   constructor({ node, from, to, duration = 1000, delay = 4 }) {
+//     willChange = css`
+//       will-change: auto;
+//       transition: background-position ${duration}ms;
+//     `
+//     node.classList.add(nodeClass)
+//     this.maxLen = maxLenNum(from, to || 0)
+//     this.node = node
+//     this.displaing = num2PadNumArr(from, this.maxLen)
+//     this.from = from
+//     this.initDigis()
+//     if (to) setTimeout(() => {
+//       this.flipTo(to)
+//     }, delay)
+//   }
+
+//   initDigis() {
+//     for(let i = 0; i < this.maxLen; i += 1) {
+//       const iNode = r(digiClass)()
+//       const offset = Math.floor(this.from / Math.pow(10, this.maxLen - 1 - i))
+//       iNode.style.backgroundPosition = `center -${offset % 100}em`
+//       iNode.classList.add(willChange)
+//       this[`digi${i}`] = iNode
+//       this.node.appendChild(iNode)
+//     }
+//   }
+
+//   flipTo(num) {
+//     const numArr = num2PadNumArr(num, this.maxLen)
+//     for(let i = 0; i < this.maxLen; i += 1) {
+//       const offset = Number(numArr.slice(0, i + 1).join(''))
+//       this[`digi${i}`].style.backgroundPosition = `center -${offset % 100}em`
+//     }
+//   }
+// }
