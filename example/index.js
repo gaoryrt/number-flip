7e048d97955ac7b8456d0a193f977ff33f2c4cc1
日天 <gaoryrt@users.noreply.github.com>
2018-06-19 14:33:03 +0800

    🔨 refactor

--- example/index.js
+++ example/index.js
@@ -1,36 +1,9 @@
 import './main.css'
+import Flip from '../flip'
 
-const duration = 4000
-const dest = 32
-
-
-const elem = document.querySelector('.ctnr')
-
-
-// const ease = pos => {
-//   if ((pos/=0.5) < 1) return 0.5*Math.pow(pos,3);
-//   return 0.5 * (Math.pow((pos-2),3) + 2);
-// }
-
-const easeFn = pos => (pos /= .5) < 1
-  ? .5 * Math.pow(pos, 3)
-  : .5 * (Math.pow((pos - 2), 3) + 2)
-const draw = percent => {
-  const translateY = - easeFn(percent) * 50 * dest % 500
-  elem.style.transform = `translateY(${translateY}px)`
-}
-const done = () => {
-  elem.style.transform = `translateY(${50 * dest % 500}px)`
-}
-
-
-
-
-const start = performance.now()
-requestAnimationFrame(function tick(now) {
-  let elapsed = now - start
-  draw(elapsed / duration)
-  if (elapsed < duration) requestAnimationFrame(tick)
-  else done()
+const el = new Flip({
+  node: document.querySelector('.app'),
+  from: 377
 })
 
+el._change(612)
