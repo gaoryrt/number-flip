d4b3ac146ccf9a1e3a3519dc9ec6fef1874ff1d7
日天 <gaoryrt@users.noreply.github.com>
2018-09-19 19:48:36 +0800

    ✨ seperator added
     #7

--- example/index.js
+++ example/index.js
@@ -4,7 +4,7 @@ const $ = s => document.querySelector(s)
 
 const flip = new Flip({
   node: $('.flip'),
-  from: 725,
+  from: 7258829,
   easeFn: function(pos) {
     if ((pos/=0.5) < 1) return 0.5*Math.pow(pos,3);
     return 0.5 * (Math.pow((pos-2),3) + 2);
@@ -13,7 +13,7 @@ const flip = new Flip({
 })
 
 $('button').onclick = () => {
-  const num = ~~(Math.random() * 999)
+  const num = ~~(Math.random() * 9999999)
   $('.num').innerText = num
   flip.flipTo({to: num, direct: true})
 }
