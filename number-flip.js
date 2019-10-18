7e0fb72338f3eadaa5900f5843fbd0460910f779
日天 <gaoryrt@users.noreply.github.com>
2019-10-18 18:12:29 +0800

    🔨 changing structure

--- number-flip.js
+++ number-flip.js
@@ -20,8 +20,8 @@ export class Flip {
     duration = .5,
     delay,
     easeFn = (pos => (pos /= .5) < 1
-                ? .5 * Math.pow(pos, 3)
-                : .5 * (Math.pow((pos - 2), 3) + 2)),
+              ? .5 * Math.pow(pos, 3)
+              : .5 * (Math.pow((pos - 2), 3) + 2)),
     systemArr = [0,1,2,3,4,5,6,7,8,9],
     direct = true,
     separator,
@@ -43,8 +43,8 @@ export class Flip {
     this.separateEvery = seperateOnly ? 0 : separateEvery
     this._initHTML(maxLenNum(this.from, this.to))
     if (to === undefined) return
-    if (delay) setTimeout(() => this.flipTo({to: this.to, direct}), delay * 1000)
-    else this.flipTo({to: this.to, direct})
+    if (delay) setTimeout(() => this.flipTo({to: this.to}), delay * 1000)
+    else this.flipTo({to: this.to})
   }
 
   _initHTML(digits) {
@@ -67,9 +67,7 @@ export class Flip {
         (!this.separateEvery && !this.seperateOnly) ||
         i === digits - 1 ||
         ((digits - i) % this.separateEvery != 1 && digits - i - this.seperateOnly != 1)
-      ) {
-        continue;
-      }
+      ) continue
       const sprtrStr = isstr(this.separator) ? this.separator : this.separator.shift()
       const sprtr = g('sprtr')(sprtrStr)
       sprtr.style.display = 'inline-block'
@@ -87,7 +85,7 @@ export class Flip {
 
   _draw({ per, alter, digit }) {
     if (this.height !== this.ctnrArr[0].clientHeight / (this.systemArr.length + 1)) {
-      this.height = this.ctnrArr[0].clientHeight / (this.systemArr.length + 1);
+      this.height = this.ctnrArr[0].clientHeight / (this.systemArr.length + 1)
     }
     const from = this.beforeArr[digit]
     const modNum = ((per * alter + from) % 10 + 10) % 10
@@ -96,44 +94,46 @@ export class Flip {
     this.ctnrArr[digit].style.transform = translateY
   }
 
+  frame(per) {
+    let temp = 0
+    for (let d = this.ctnrArr.length - 1; d >= 0; d -= 1) {
+      let alter = this.afterArr[d] - this.beforeArr[d]
+      temp += alter
+      this._draw({
+        digit: d,
+        per: this.easeFn(per),
+        alter: this.direct ? alter : temp
+      })
+      temp *= 10
+    }
+  }
+
   flipTo({
     to,
     duration,
     easeFn,
-    direct = true
+    direct
   }) {
+    if (easeFn) this.easeFn = easeFn
+    if (direct !== undefined) this.direct = direct
     const len = this.ctnrArr.length
     this.beforeArr = num2PadNumArr(this.from, len)
     this.afterArr = num2PadNumArr(to, len)
-    const draw = per => {
-      let temp = 0
-      for (let d = this.ctnrArr.length - 1; d >= 0; d -= 1) {
-        let alter = this.afterArr[d] - this.beforeArr[d]
-        temp += alter
-        const fn = easeFn || this.easeFn
-        this._draw({
-          digit: d,
-          per: fn(per),
-          alter: direct ? alter : temp
-        })
-        temp *= 10
-      }
-    }
     const start = Date.now()
     const dur = (duration * 1000) || this.duration
     const tick = () => {
       let elapsed = Date.now() - start
-      draw(elapsed / dur)
+      this.frame(elapsed / dur)
       if (elapsed < dur) requestAnimationFrame(tick)
       else {
         this.from = to
-        draw(1)
+        this.frame(1)
       }
     }
     window.addEventListener('resize', () => {
       this.height = this.ctnrArr[0].clientHeight / (this.systemArr.length + 1)
       this.node.style.height = this.height + 'px'
-      draw(1)
+      this.frame(1)
     })
     requestAnimationFrame(tick)
   }
