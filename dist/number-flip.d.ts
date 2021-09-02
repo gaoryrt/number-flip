3fbf30151b011d3000c843e031c84f92a95820c9
Artur <artu-ole@yandex.ru>
2021-09-02 12:50:39 +0300

    convert to typescript. remove gelerator dependency

---
+++ dist/number-flip.d.ts
@@ -0,0 +1,45 @@
+interface FlipOptions {
+    node: HTMLElement;
+    from: number;
+    to?: number;
+    duration?: number;
+    delay?: number;
+    easeFn?: (pos: number) => number;
+    systemArr?: Array<string | number>;
+    direct?: boolean;
+    separator?: string | string[];
+    separateOnly?: number;
+    separateEvery?: number;
+}
+export declare class Flip {
+    private beforeArr;
+    private afterArr;
+    private ctnrArr;
+    private duration;
+    private systemArr;
+    private easeFn;
+    from: number;
+    to: number;
+    private node;
+    private direct;
+    private separator?;
+    private separateOnly;
+    private separateEvery;
+    private height?;
+    constructor({ node, from, to, duration, delay, easeFn, systemArr, direct, separator, separateOnly, separateEvery, }: FlipOptions);
+    _initHTML(digits: number): void;
+    _draw({ per, alter, digit }: {
+        per: number;
+        alter: number;
+        digit: number;
+    }): void;
+    frame(per: number): void;
+    flipTo({ to, duration, easeFn, direct, }: {
+        to: number;
+        duration?: number;
+        easeFn?: () => any;
+        direct?: boolean;
+    }): void;
+    setSelect(num: any): void;
+}
+export {};
